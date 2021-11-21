5a2c43894a2f32be9e4bbbd4670be34f49b8d2d3
giladgafny1 <giladgafny@campus.technion.ac.il>
2021-11-21 16:49:15 +0200

    creating

---
+++ cmake-build-debug/CMakeFiles/DataStructurs_hw1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataStructurs_hw1.dir/main.cpp.obj"
+  "DataStructurs_hw1.exe"
+  "DataStructurs_hw1.exe.manifest"
+  "DataStructurs_hw1.pdb"
+  "libDataStructurs_hw1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DataStructurs_hw1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/DataStructurs_hw1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DataStructurs_hw1.
+# This may be replaced when dependencies are built.
